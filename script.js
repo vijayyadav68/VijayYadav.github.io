89a390edf2fbcae753bd415bc1300957e930f7ae
vijayyadav68 <vij99368@gmail.com>
2024-10-23 17:21:35 +0530

    Vijay
    Vijay
    
    Signed-off-by: vijayyadav68 <vij99368@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-
-
-
-
-/*======================== scroll reveal =========================*/
 ScrollReveal({ 
     reset: true,
     distance: '80px',
@@ -14,7 +8,7 @@ ScrollReveal({
 
 
 
-ScrollReveal().reveal('.home-text, .about-text, .main-text, .contect h1', { origin: 'top' });
-ScrollReveal().reveal('.image, .skill-main, .contect', { origin: 'bottom' });
-ScrollReveal().reveal('.home-text h1, .about-text h1', { origin: 'left' });
-ScrollReveal().reveal('.home-text p, .about-text p', { origin: 'right' });
+ScrollReveal().reveal('.home .content h3', { origin: 'top' });
+ScrollReveal().reveal('.img, .Vloge, .container', { origin: 'bottom' });
+ScrollReveal().reveal('.content h2, .about-text h1', { origin: 'left' });
+ScrollReveal().reveal('.content p, .about-text p', { origin: 'right' });
